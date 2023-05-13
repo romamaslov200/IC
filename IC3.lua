9d4d38b8c5db6670ba3a39a9801ff94d5f5809b9
Roman <81181606+romamaslov200@users.noreply.github.com>
2023-05-13 15:59:32 +0300

    Add files via upload
    asd

--- IC3.lua
+++ IC3.lua
@@ -17,10 +17,13 @@ local ini = inicfg.load(inicfg.load({
     main = {
         stroka = 1,
         delay = 0,
-        key = "Key"
+        key = "Key",
+        version = "1"
     },
 }, directIni))
 inicfg.save(ini, directIni)
+
+script_version(ini.main.version)
 
 local sizeX, sizeY = getScreenResolution()
 
@@ -38,10 +41,70 @@ local started = 0
 
 local key_active = false
 
+
+
+function autoupdate(json_url, prefix, url)
+  local dlstatus = require('moonloader').download_status
+  local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
+  if doesFileExist(json) then os.remove(json) end
+  downloadUrlToFile(json_url, json,
+    function(id, status, p1, p2)
+      if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+        if doesFileExist(json) then
+          local f = io.open(json, 'r')
+          if f then
+            local info = decodeJson(f:read('*a'))
+            updatelink = url
+            updateversion = info.latest
+            f:close()
+            os.remove(json)
+            if updateversion ~= thisScript().version then
+              lua_thread.create(function(prefix)
+                local dlstatus = require('moonloader').download_status
+                local color = -1
+                sampAddChatMessage((prefix..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
+                wait(250)
+                downloadUrlToFile(updatelink, thisScript().path,
+                  function(id3, status1, p13, p23)
+                    if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+                      print(string.format('Загружено %d из %d.', p13, p23))
+                    elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+                      print('Загрузка обновления завершена.')
+                      sampAddChatMessage((prefix..'Обновление завершено!'), color)
+                      goupdatestatus = true
+                      lua_thread.create(function() wait(500) thisScript():reload() end)
+                    end
+                    if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+                      if goupdatestatus == nil then
+                        sampAddChatMessage((prefix..'Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
+                        update = false
+                      end
+                    end
+                  end
+                )
+                end, prefix
+              )
+            else
+              update = false
+              print('v'..thisScript().version..': Обновление не требуется.')
+            end
+          end
+        else
+          print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url)
+          update = false
+        end
+      end
+    end
+  )
+  while update ~= false do wait(100) end
+end
+
+
 function main()
     if not isSampfuncsLoaded() or not isSampLoaded() then return end --
     sampRegisterChatCommand('ic', ic)
     while not isSampAvailable() do wait(0) end -- 
+    autoupdate("https://raw.githubusercontent.com/romamaslov200/IC/main/version.json", '['..string.upper(thisScript().name)..']: ', "https://raw.githubusercontent.com/romamaslov200/IC/main/IC3.lua")
     while true do --
         wait(0) --
 
